da61c0507c227019758f3c3101491ba2457caa32
princebyun <princebyun@gmail.com>
2024-03-18 15:41:10 +0900

    DailySaint 커밋

---
+++ src/main/java/ToyProject/DailySaint/DailySaintApplication.java
@@ -0,0 +1,13 @@
+package ToyProject.DailySaint;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DailySaintApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DailySaintApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ToyProject/DailySaint/DailySaintApplicationTests.java
@@ -0,0 +1,13 @@
+package ToyProject.DailySaint;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DailySaintApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
